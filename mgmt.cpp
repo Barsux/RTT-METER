b166a43c75d2ad86df505e64c32d481fdfd6a348
Barsux <yity.r7@gmail.com>
2021-11-19 00:01:43 +0300

    Comment some troubles

--- mgmt.cpp
+++ mgmt.cpp
@@ -8,35 +8,43 @@ public:
         IP4 srcIP, dstIP;
         int scrPORT, dstPORT;
         int size, pckt_per_s, duration;
-        pacekt(){
-            memset(srcMAC, 0, sizeof(srcMAC));
-            memset(dstMAC, 0, sizeof(dstMAC));
-            memset(srcIP, 0, sizeof(srcIP));
-            memset(dstIP, 0, sizeof(dstIP));
-            scrPORT(), dstPORT(), size(1024), pckt_per_s(1), duration(1);
+        pckt(): scrPORT(5850), dstPORT(5850), size(1024), pckt_per_s(1), duration(1) {
+            //memset(srcMAC, 0, sizeof(srcMAC));
+            //memset(dstMAC, 0, sizeof(dstMAC));
+            //memset(srcIP, 0, sizeof(srcIP));
+            //memset(dstIP, 0, sizeof(dstIP));
         }
     }pckt;
 
     bool converted;
     class Job: public Queue_job {public:
         MgmtObject &base;
-        struct pckt convert(char* argv[], int argc){
-            return base.convert(char* argv[], int argc);
-        }
+        Job(MgmtObject &base): base(base){}
+        //struct pckt convert(int argc, char **argv){
+        //    return base.convert(int argc, char **argv);
+        //}
     } mgmt_job;
 
     class Report: public Queue_report {public:
-        void report()
+        MgmtObject &base;
+        Report(MgmtObject &base): base(base){}
+        void report(){
+            return base.report_void();
+        }
     } mgmt_report;
     MgmtObject(WaitSystem* waitSystem, Mgmt::Setup &setup): WaitSystem::Module(waitSystem)
     , setup(setup), converted(false), mgmt_job(*this), mgmt_report(*this)
     {
         module_debug = "MGMT";
         job = &mgmt_job;
-        report = &mgmt_report; enable_wait(mgmt_report);
+        report = &mgmt_report; enable_wait(report);
     }
-    pckt convert(){
-        return
+    void report_void(){
+
+    }
+    struct pckt convert(){
+        struct pckt data;
+        return data;
     }
     void evaluate(){
         if(!converted) convert();
