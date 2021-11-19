a96af99834e1cf6e207a7c80d7a9538dbaa4415b
Barsux <yity.r7@gmail.com>
2021-11-19 18:38:19 +0300

    Added File_mgmt init

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include "core.h"
 #include "l2_transport_linux.h"
 #include "mgmt.h"
+#include "file_mgmt.h"
 
 #pragma argsused
 int main(int argc, char **argv)
 {
   WaitSystem* waitSystem = new_WaitSystem();
+
   Core::Setup coreSetup;
   Core* core = new_Core(waitSystem, coreSetup);
 
@@ -14,11 +16,15 @@ int main(int argc, char **argv)
   memcpy(mgmtSetup.argv, argv, sizeof(argv));
   mgmtSetup.argc = argc;
   Mgmt* mgmt = new_Mgmt(waitSystem, mgmtSetup);
-  core->attach_mgmt(mgmt->job, mgmt->report);
 
   L2Transport::Setup l2Transport_setup;
   l2Transport_setup.physicalId = "enp4s0";
   L2Transport* l2Transport = new_L2Transport(waitSystem, l2Transport_setup);
+
+  File_mgmt::Setup file_mgmt_setup;
+  file_mgmt_setup.path = "PATH_TO_CFG";
+
+  core->attach_mgmt(mgmt->job, mgmt->report);
   core->attach_l2_transport(l2Transport->rx, l2Transport->tx, l2Transport->sent);
 
 
