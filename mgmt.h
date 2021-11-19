e6bec8093b77d0693d7c7ec361ec375f5a95c5e1
Barsux <yity.r7@gmail.com>
2021-11-19 15:09:09 +0300

    Struct replaced to mgmt header

--- mgmt.h
+++ mgmt.h
@@ -5,12 +5,20 @@
 #include "base.h"
 
 class Mgmt{public:
+    struct pckt{
+        MAC srcMAC, dstMAC;
+        IP4 srcIP, dstIP;
+        int scrPORT, dstPORT;
+        int size, pckt_per_s, duration;
+        pckt(): scrPORT(5850), dstPORT(5850), size(1024), pckt_per_s(1), duration(1) {}
+    }pckt;
     virtual ~Mgmt() {}
     class Setup {public:
         int argc;
         char* argv[];
     };
     class Queue_job: public WaitSystem::Queue {public:
+        struct pckt packet;
     }* job;
     class Queue_report: public WaitSystem::Queue {public:
     }* report;
