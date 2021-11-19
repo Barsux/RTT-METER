c7aedb0029a28743eca1e285b43d1914503e0895
Barsux <yity.r7@gmail.com>
2021-11-19 18:40:31 +0300

    Fixed some issues

--- file_mgmt.h
+++ file_mgmt.h
@@ -2,12 +2,33 @@
 #define FILE_MGMT_H
 #include "base.h"
 class File_mgmt{public:
+    struct settings{
+        bool have_settings;
+        struct mgmt{
+            bool busy;
+            mgmt(): busy(false){}
+        }mgmt;
+        struct packager{
+            bool busy;
+            int port;
+            char * interface;
+            packager(): busy(false){}
+        }packager;
+        struct core{
+            bool busy;
+            core(): busy(false){}
+        }core;
+    settings(): have_settings(false){}
+    }settings;
     virtual ~File_mgmt() {}
     class Setup {public:
+        char * path;
     };
     class Queue_toSet: public WaitSystem::Queue {public:
+        struct settings config;
     }* set;
     class Queue_toSave: public WaitSystem::Queue {public:
+        struct settings config;
     }* save;
 };
 
